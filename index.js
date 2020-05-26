8d9d3f2013ac2de14e329810542797d642056680
Cheryl Daniels <cheryld433@gmail.com>
2020-05-26 10:02:16 -0400

    updated files

---
+++ index.js
@@ -0,0 +1,41 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const consoleTable = require("console.table");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Dixiedog1",
+  database: ""
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  runSearch();
+});
+
+function runSearch(){
+
+}
+
+// Switch statement:
+
+//Add departments, roles, and employees:
+
+// View departments, roles, and employees:
+
+// Update employee roles:
+
+// BONUS....
+// Update  Employee managers:
+// View employees by manager:
+// Delete departments, roles, and employees:
+// View the total utilized budget of a department( combined salaries of all employees in the department)
+
