f85b5de1cbc1cd393162a410e6d440dff0289faf
Cheryl Daniels <cheryld433@gmail.com>
2020-05-26 10:16:13 -0400

    added inquirer function

--- index.js
+++ index.js
@@ -22,6 +22,31 @@ connection.connect(function(err) {
 });
 
 function runSearch(){
+    inquirer
+    .prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "Add Employee:",
+            "Add Department:",
+            "Add Role:",
+            "View Department:",
+            "View Roles:",
+            "View Employee:",
+            "Update Employee Role:",
+            "Update Employee Manager:",
+            "View Employee by Manager:",
+            "Delete Employee:",
+            "Delete Department:",
+            "Delete Role:",
+            "View Department Budget:",
+            "Exit"
+        ]
+    })
+    .then(function(answer){
+        
+    })
 
 }
 
